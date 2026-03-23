fa16f77cdce2c7a732c583ef865e5e4ba3c3c7c6
Balogun Elizabeth <119706596+Olatilewaoluwa@users.noreply.github.com>
2026-03-23 19:30:51 +0100

    Create project.js

---
+++ pages/project.js
@@ -0,0 +1,12 @@
+export default function Project() {
+  return (
+    <iframe
+      src="/index (2).html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+    />
+  );
+}
